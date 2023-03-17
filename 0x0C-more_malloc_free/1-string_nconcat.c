244be5fc55768c1774c2ee2b3fb3ee54897d5f79
osadebe2 <osadebegoldenboy2@gmail.com>
2023-03-17 10:04:18 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,58 +1,52 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- *_strlen - counts and returns string length
- * @s: the input string
- *
- * Return: the length
- */
-
-int _strlen(char *s)
-{
-	int c = 0;
-
-	for (c = 0; *s; c++)
-		s++;
-
-	return (c);
-}
-
-/**
- * string_nconcat - concatenates 2 strings
- * @s1: the first str
- * @s2: the second str
- * @n: how much of s2 to add
- *
- * Return: ptr to new string
- */
-
+  * string_nconcat - ...
+  * @s1: ...
+  * @s2: ...
+  * @n: ...
+  *
+  * Return: ...
+  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *nstr;
-	unsigned int idx, jdx, s1l, s2l;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
+	char *str;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	s1l = _strlen(s1);
-	s2l = _strlen(s2);
+	while (s1[i])
+		i++;
 
-	n = (n >= s2l) ? s2l : n;
+	while (s2[k])
+		k++;
 
-	nstr = malloc((s1l + n) * sizeof(char) + 1);
-	if (!nstr)
+	if (n >= k)
+		l = i + k;
+	else
+		l = i + n;
+
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
 		return (NULL);
 
-	for (idx = 0; idx < s1l; idx++)
-		nstr[idx] = s1[idx];
+	k = 0;
+	while (j < l)
+	{
+		if (j <= i)
+			str[j] = s1[j];
 
-	for (jdx = 0; jdx < n; jdx++, idx++)
-		nstr[idx] = s2[jdx];
-
-	nstr[idx] = '\0';
-	return (nstr);
+		if (j >= i)
+		{
+			str[j] = s2[k];
+			k++;
+		}
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
 }
