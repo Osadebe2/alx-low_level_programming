d4beb225a3cc0fa8613e778e734b68c8f51014c5
osadebe2 <osadebegoldenboy2@gmail.com>
2023-04-03 12:55:12 -0700

    0-print_listint.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _atoi - Entry point
- * @s: input
- * Return: Always 0 (Success)
- */
+* _atoi - Entry point
+* @s: input
+* Return: Always 0 (Success)
+/
 int _atoi(char *s)
 {
 
